1416038cdb42754a956fd7da5b00eccf87a36d30
YADAV, SHIVANAND <shivanandyadav12a@gmail.com>
2024-09-21 16:40:34 +0530

    add jitpack and maven setup

--- SentinelArmor/build.gradle.kts
+++ SentinelArmor/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -24,11 +25,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
+    }
+}
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
